ea9b359785cabd24c624ec51de0ea25b0bb5f6f3
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-11-26 09:14:12 -0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
-    // TODO: create a named branch named after your firstname-lastname (e.g. justin-reich)
     // TODO: add a main method and sout out your name
+    public static void main(String[] args) {
+        System.out.println("KC McInturf");
+    }
     // TODO: configure your IDE appropriately to run the main method...
         // right click on the src folder and mark it as the sources root
         // click cmd + ; to open the "Project Structure" menu
