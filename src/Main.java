94f7b452481d478b03c4498b2dbea48591868db0
davidandstephens <stephens.a.david@gmail.com>
2018-11-26 10:57:52 -0600

    made sure to keep TODOne convention consistant

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     // TODOne: create a named branch named after your firstname-lastname (e.g. justin-reich)
     // TODOne: add a main method and sout out your name
-    // TODO: configure your IDE appropriately to run the main method...
+    // TODOne: configure your IDE appropriately to run the main method...
         // right click on the src folder and mark it as the sources root
         // click cmd + ; to open the "Project Structure" menu
         // in "Project Structure" select a Java SDK version
         // in "Project Structure" define a folder for compiled classes called "out" just inside your project root folder
-    // TODO: add and commit your code and push your branch up to GitHub
+    // TODOne: add and commit your code and push your branch up to GitHub
     public static void main(String[] args) {
         System.out.println("David Andrew Stephens");
     }
