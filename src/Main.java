35b498e29cd437f7b3690368ab396deb8d4da640
benny-fields3rd <benny.fields3rd@gmail.com>
2018-11-26 10:57:42 -0600

    completed warmup

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     // TODO: create a named branch named after your firstname-lastname (e.g. justin-reich)
     // TODO: add a main method and sout out your name
+    public static void main( String[] args ) {
+        System.out.println("Benny Fields");
+    }
     // TODO: configure your IDE appropriately to run the main method...
         // right click on the src folder and mark it as the sources root
         // click cmd + ; to open the "Project Structure" menu
